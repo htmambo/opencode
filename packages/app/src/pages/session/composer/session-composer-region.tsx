dc9dcc3c8f141ba80708c9df1282e3d38584501d
果农 <htmambo@gmail.com>
2026-02-28 23:13:03 +0800

    max width

--- packages/app/src/pages/session/composer/session-composer-region.tsx
+++ packages/app/src/pages/session/composer/session-composer-region.tsx
@@ -33,7 +33,7 @@ export function SessionComposerRegion(props: {
       <div
         classList={{
           "w-full px-3 pointer-events-auto": true,
-          "md:max-w-200 md:mx-auto 2xl:max-w-[1000px]": controller.centered(),
+          "md:mx-auto": props.centered,
         }}
       >
         <Show when={controller.state.questionRequest()} keyed>
